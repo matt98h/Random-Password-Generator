8057f6437f520cdf82e500111fba675b55bd9339
Matthew Huus <matthew.huus3@gmail.com>
2020-06-12 16:25:53 -0500

    working program. cleaned up the error message, and finished the generate password function.

--- Develop/script.js
+++ Develop/script.js
@@ -4,30 +4,32 @@ function writePassword(){
   // set password equal to the value of our generated password
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
-  passwordText.value = password;
+
+  passwordText.textContent = password;
+  
 }
 // all of the variables for our character sets
 var charsetLowercaseOnly = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     var charsetUppercaseOnly = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     var charsetNumbersOnly = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     var charsetspecialCharsOnly = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '\',', '?', '/', '>', '.', '<', '`', '~', ';', ':', '{', '}'];
-
 // Prompt the user Asking how many characters they want their password to be. Needs to be between 8-128 characters, if not throw error. 
 function generatePassword() {
   var passwordLength = prompt("How many characters do you want your password to be?");
-  console.log(passwordLength);
+  
   if (passwordLength < 8 || passwordLength > 128 || passwordLength == null) {
 
-    alert("Oops! Please refresh the page and pick a length between 8 and 128.");
+    alert("Oops! Please click Generate Password and pick a length between 8 and 128.");
 
 
     // Then use a bunch of confirms for the requirements that they want for their random password(lowercase,uppercase,numeric,special chars).
   } else {
     var useLowercase = confirm("Do you want to have lowercase letters in your password? (Ok = yes and cancel = no)");
-    var useUppercase = confirm("Do you want to have uppercase letters in your password?");
-    var useNumbers = confirm("Do you want to have numbers in your password?");
-    var useSpecialChars = confirm("Do you want to have special characters in your password?");
+    var useUppercase = confirm("Do you want to have uppercase letters in your password? (Ok = yes and cancel = no)");
+    var useNumbers = confirm("Do you want to have numbers in your password? (Ok = yes and cancel = no)");
+    var useSpecialChars = confirm("Do you want to have special characters in your password? (Ok = yes and cancel = no)");
 
 
     /* We need to genreate a password utilizing random number, character, and special character generators. We do that by first selecting which random generator we are going 
@@ -57,15 +59,15 @@ function generatePassword() {
         var randomChar = finalCharset[Math.floor(Math.random() * finalCharset.length)]
 
         generatedPassword = generatedPassword + randomChar;
-        console.log(generatedPassword);
+
+       
       }
     }
-
+    return generatedPassword;
   }
-return password;
 }
 generateBtn.addEventListener("click",writePassword);
-
+console.log(window);
 
 
 
